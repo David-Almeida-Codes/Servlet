a8a6d61f0c7efee6865ebec5e0b71f8738b6a895
David Almeida <davidsouzacontato@gmail.com>
2019-12-17 01:10:59 -0300

    init

---
+++ src/br/cm/alura/gerenciador/servlet/OiMundoServLet.java
@@ -0,0 +1,23 @@
+package br.cm.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServLet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body> Oi mundo, eu sou um servlet! </body></html>");
+		
+	}
+
+}
