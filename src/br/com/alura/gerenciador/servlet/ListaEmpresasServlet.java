8f3b65e61084266071fbc8f1731a644ca56d9434
David Almeida <davidsouzacontato@gmail.com>
2019-12-17 14:51:13 -0300

    Definindo o nosso modelo

---
+++ src/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -0,0 +1,48 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListaEmpresasServlet
+ */
+@WebServlet("/listaEmpresas")
+public class ListaEmpresasServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListaEmpresasServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Banco banco = new Banco();
+		List<Empresa> lista = banco.getEmpresas();
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>"); 
+		out.println("<ul>");
+		for(Empresa empresas : lista) {
+			out.println("<li>" + empresas.getNome() + "</li>");
+		}
+		out.println("</ul>");
+		out.println("</body></html>");
+		
+		
+	}
+
+}
